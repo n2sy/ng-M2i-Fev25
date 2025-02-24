195b4ce047bb20087c99ff6762ee3db09132f6c5
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-02-24 11:28:53 +0100

    first commit

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  standalone: true,
+  imports: [],
+  templateUrl: './first.component.html',
+})
+export class FirstComponent {
+
+}
