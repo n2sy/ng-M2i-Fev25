72545ae7843f3ac5e8ad3b2d887cef4440c376a7
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-02-24 11:55:36 +0100

    binding types

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-first',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './first.component.html',
 })
 export class FirstComponent {
+  prenom: string = 'nidhal';
+  nom: string = 'jelassi';
+  couleurPrefere = 'yellow';
+  hd = true;
 
+  clickHandler() {
+    alert("J'ai été cliqué");
+  }
+
+  changeValueInput(inp: any) {
+    this.prenom = inp.value;
+  }
 }
